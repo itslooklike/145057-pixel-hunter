d80bdd1745f80b93f95da1b32df897cd043b27f6
Alexey Masalov <delsol@bk.ru>
2018-01-21 01:31:12 +0300

    add more pics

--- js/data/state.js
+++ js/data/state.js
@@ -5,11 +5,21 @@ export const urlMocks = {
     `https://k42.kn3.net/CF42609C8.jpg`,
     `https://k42.kn3.net/D2F0370D6.jpg`,
     `https://k32.kn3.net/5C7060EC5.jpg`,
+    `http://www.reading.com.ua/wp-content/uploads/2015/02/portrety-devushek-750x644.jpg`,
+    `http://foto.fastimagess.ru/img-q5y5x5n4g4041456w4t4q2q5u5f5d4c4s404h4e4p4s4g58406a4p4q2o4u4/2014/06/kak_narisovat_orhideu_cvetnymi_karandashami36.jpg`,
+    `https://w-dog.ru/wallpapers/15/3/437850330584583/lev-cvetnoj-vzglyad.jpg`,
+    `http://boombob.ru/img/picture/Oct/12/b8be558f2365931a3c8100097dfe7e30/7.jpg`,
+    `http://www.krasfun.ru/images/2014/9/68357_3d-pencil-drawings-024.jpg`,
   ],
   photos: [
     `http://i.imgur.com/1KegWPz.jpg`,
     `https://i.imgur.com/DiHM5Zb.jpg`,
     `http://i.imgur.com/DKR1HtB.jpg`,
+    `https://million-wallpapers.ru/wallpapers/1/50/small/339646622751917.jpg`,
+    `https://dncache-mauganscorp.netdna-ssl.com/thumbseg/2201/2201971-bigthumbnail.jpg`,
+    `http://ticketforplane.ru/sites/default/files/styles/350pxteaser/public/field/image/new-york.jpg?itok=yI93WXkN`,
+    `http://i0.wallpaperscraft.ru/image/gonkong_gorod_noch_peyzazh_725_300x175.jpg`,
+    `https://dncache-mauganscorp.netdna-ssl.com/thumbseg/1654/1654645-bigthumbnail.jpg`,
   ],
 };
 
